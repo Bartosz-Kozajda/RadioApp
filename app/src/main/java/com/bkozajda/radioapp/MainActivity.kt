d5432152e05f065622f72cdb46a79b22d89829e2
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-02-11 20:18:23 +0100

    Initial commit

---
+++ app/src/main/java/com/bkozajda/radioapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bkozajda.radioapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

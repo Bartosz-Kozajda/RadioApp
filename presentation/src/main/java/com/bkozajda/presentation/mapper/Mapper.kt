60cac1b55309ca0b57bcca0d2b65d47ce992b179
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-06-17 19:33:01 +0200

    Remove presentation module

--- presentation/src/main/java/com/bkozajda/presentation/mapper/Mapper.kt
+++
@@ -1,5 +0,0 @@
-package com.bkozajda.presentation.mapper
-
-interface Mapper<out V, in D> {
-    fun mapToView(type: D): V
-}
--- presentation/src/main/java/com/bkozajda/presentation/model/MovieViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.bkozajda.presentation.model
-
-data class MovieViewModel(
-    val id: Int,
-    val title: String,
-    val voteAverage: Double,
-    val posterPath: String
-)

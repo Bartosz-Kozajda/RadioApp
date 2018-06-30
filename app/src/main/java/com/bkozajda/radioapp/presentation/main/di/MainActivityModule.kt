3e8dc7dff7fc26217b2c903b47c1b798b9050e1f
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-06-30 16:14:11 +0200

    Add custom scopes

--- app/src/main/java/com/bkozajda/radioapp/presentation/main/di/MainActivityModule.kt
+++ app/src/main/java/com/bkozajda/radioapp/presentation/main/di/MainActivityModule.kt
@@ -1,5 +1,6 @@
 package com.bkozajda.radioapp.presentation.main.di
 
+import com.bkozajda.radioapp.di.scopes.PerFragment
 import com.bkozajda.radioapp.presentation.discover.DiscoverMoviesFragment
 import com.bkozajda.radioapp.presentation.discover.di.DiscoverMoviesFragmentModule
 import dagger.Module
@@ -7,6 +8,7 @@ import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class MainActivityModule {
+    @PerFragment
     @ContributesAndroidInjector(modules = [(DiscoverMoviesFragmentModule::class)])
     abstract fun discoverMoviesFragmentInjector(): DiscoverMoviesFragment
 }

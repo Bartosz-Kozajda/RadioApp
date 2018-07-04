63980ffabed021fa4729a590a0cbcc2b62345cb0
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-04 19:29:34 +0200

    Add glide

--- remote/src/main/java/com/bkozajda/remote/mapper/MovieEntityMapper.kt
+++ remote/src/main/java/com/bkozajda/remote/mapper/MovieEntityMapper.kt
@@ -6,6 +6,7 @@ import javax.inject.Inject
 
 open class MovieEntityMapper @Inject constructor() : EntityMapper<MovieModel, MovieEntity> {
     override fun mapFromRemote(type: MovieModel): MovieEntity {
-        return MovieEntity(type.id, type.title, type.voteAverage, type.posterPath)
+        val posterPath = "https://image.tmdb.org/t/p/w500${type.posterPath}"
+        return MovieEntity(type.id, type.title, type.voteAverage, posterPath)
     }
 }

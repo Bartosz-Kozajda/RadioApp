8a451649276851908abaf5addf17fbbb6030ecdf
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-11 19:47:24 +0200

    Fix typo

--- app/src/main/java/com/bkozajda/radioapp/UiThread.kt
+++ app/src/main/java/com/bkozajda/radioapp/UiThread.kt
@@ -5,7 +5,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
 
 class UiThread : PostExecutionThread {
-
     override val scheduler: Scheduler
         get() = AndroidSchedulers.mainThread()
 }

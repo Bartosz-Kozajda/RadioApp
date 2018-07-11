eff1b87946771aaac5521ec95a35f0fed32d16fe
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-11 20:37:48 +0200

    Fix dryrun test

--- app/src/test/java/com/bkozajda/radioapp/KoinDryRunTest.kt
+++ app/src/test/java/com/bkozajda/radioapp/KoinDryRunTest.kt
@@ -3,12 +3,22 @@ package com.bkozajda.radioapp
 import com.bkozajda.radioapp.di.AppModule
 import com.bkozajda.radioapp.presentation.discover.di.DiscoverMoviesFragmentModule
 import com.bkozajda.radioapp.presentation.movieDetail.di.MovieDetailFragmentModule
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
 import org.koin.standalone.StandAloneContext.startKoin
 import org.koin.test.KoinTest
 import org.koin.test.dryRun
 
+
 class KoinDryRunTest : KoinTest {
+
+    @Before
+    fun setup() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+    }
+
     @Test
     fun `dryRun module`() {
         startKoin(listOf(AppModule, DiscoverMoviesFragmentModule, MovieDetailFragmentModule))

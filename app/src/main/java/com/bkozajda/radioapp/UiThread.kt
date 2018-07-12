e7e1be9498a5580a7e9030d0698a6176546b64c7
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-12 21:36:39 +0200

    Simplify schedulers

--- app/src/main/java/com/bkozajda/radioapp/UiThread.kt
+++ app/src/main/java/com/bkozajda/radioapp/UiThread.kt
@@ -2,7 +2,7 @@ package com.bkozajda.radioapp
 
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
-import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
+import com.bkozajda.domain.executor.PostExecutionThread
 
 class UiThread : PostExecutionThread {
     override val scheduler: Scheduler
--- domain/src/main/java/com/bkozajda/domain/usecases/base/UseCase.kt
+++ domain/src/main/java/com/bkozajda/domain/usecases/base/UseCase.kt
@@ -1,14 +1,12 @@
 package com.bkozajda.domain.usecases.base
 
+import com.bkozajda.domain.executor.PostExecutionThread
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import org.buffer.android.boilerplate.domain.executor.PostExecutionThread
-import org.buffer.android.boilerplate.domain.executor.ThreadExecutor
 
 abstract class UseCase<T, in Params>(
-    private val threadExecutor: ThreadExecutor,
     private val postExecutionThread: PostExecutionThread
 ) {
     private val disposables = CompositeDisposable()
@@ -17,7 +15,7 @@ abstract class UseCase<T, in Params>(
 
     fun execute(params: Params? = null): Observable<T> {
         return buildUseCaseObservable(params)
-                .subscribeOn(Schedulers.from(threadExecutor))
+                .subscribeOn(Schedulers.io())
                 .observeOn(postExecutionThread.scheduler) as Observable<T>
     }
 

a37ddea105b4dd5b798789280503eb04f26f59dd
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-13 21:19:01 +0200

    Setup toolbar

--- app/src/main/java/com/bkozajda/radioapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bkozajda/radioapp/presentation/main/MainActivity.kt
@@ -2,12 +2,23 @@ package com.bkozajda.radioapp.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.bkozajda.radioapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        NavigationUI.setupWithNavController(toolbar, host.navController)
     }
 }

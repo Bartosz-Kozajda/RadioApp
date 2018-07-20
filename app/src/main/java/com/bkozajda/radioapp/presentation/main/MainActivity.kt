4af8de3167e02463b09499cebd11d31fcf5f91b7
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2018-07-20 14:17:20 +0200

    Update navigation

--- app/src/main/java/com/bkozajda/radioapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bkozajda/radioapp/presentation/main/MainActivity.kt
@@ -3,7 +3,7 @@ package com.bkozajda.radioapp.presentation.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.bkozajda.radioapp.R
 import com.bkozajda.radioapp.presentation.main.presentation.MainActivityViewModel
@@ -17,17 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
         setSupportActionBar(toolbar)
-        setupNavigationDrawer(navController)
-        setupNavigationView(navController)
+        setupNavigationDrawer(navController())
+        setupNavigationView(navController())
     }
 
-    private fun setupNavigationDrawer(navController: NavController) {
+    override fun onSupportNavigateUp() =
+            navController().navigateUp()
+
+    private fun setupNavigationDrawer(navController: NavController) =
         NavigationUI.setupWithNavController(toolbar, navController, drawer_layout)
-    }
 
-    private fun setupNavigationView(navController: NavController) {
+    private fun setupNavigationView(navController: NavController) =
         NavigationUI.setupWithNavController(nav_view, navController)
-    }
+
+    private fun AppCompatActivity.navController() = findNavController(this, R.id.nav_host_fragment)
 }
